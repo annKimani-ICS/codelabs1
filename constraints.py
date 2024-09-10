539f08df4f940ecf1069c379b0ad4213e90b3083
annkimani <ann.wangari@strathmore.edu>
2024-09-10 15:14:57 +0300

    Path changed for OUTPUT_JSON variable

--- constraints.py
+++ constraints.py
@@ -1,7 +1,7 @@
 # File paths
 OUTPUT_CSV = 'data/output.csv'
 OUTPUT_TSV = 'data/output.tsv'
-OUTPUT_JSON = 'data/output.json'
+OUTPUT_JSON = 'data/shuffled_students.json'
 OUTPUT_JSONL = 'data/output.jsonl'
 LOG_FILE = 'logs/student_data.log'
 
